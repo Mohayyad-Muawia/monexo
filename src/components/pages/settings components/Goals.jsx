f8bfb346be8fab555179a27b4553becb500a9d34
Mohayyad Muawia <mohayyad2.0@gmail.com>
2024-12-29 10:15:59 +0200

    Complaining about time

--- src/components/pages/settings components/Goals.jsx
+++ src/components/pages/settings components/Goals.jsx
@@ -2,6 +2,7 @@ const Goals = () => {
   return (
     <div className="goals">
       <h1>Goals</h1>
+      <P>I am sorry to tell you that the 3 and a half days wasn't quite enough for all ouer ideas (and this page one of them), but at least we did our best :).</P>
     </div>
   );
 };
